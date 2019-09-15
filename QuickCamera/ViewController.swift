8c7a409f66d04a600986b5361cbae87c3a6803f7
Georgij Emelyanov <emelyanov@quality-wolves.com>
2019-09-15 15:23:58 +0300

    Initial Commit

---
+++ QuickCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickCamera
+//
+//  Created by Georgij Emelyanov on 15/09/2019.
+//  Copyright © 2019 Georgij Emelyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
